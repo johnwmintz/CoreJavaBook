4ef46d5bda2d7bac283f319a4b5592d205b54549
John Mintz <john.mintz@gmail.com>
2026-03-05 20:46:32 -0600

    Starting Chapter 2

---
+++ Chapter2/src/Welcome.java
@@ -0,0 +1,11 @@
+public class Welcome {
+    /**
+     * This program displays a greeting for the reader.
+     */
+    void main(){
+        String greeting = "Welcome to Core Java";
+        IO.println(greeting);
+        IO.println("=".repeat(greeting.length()));
+    }
+    
+}
